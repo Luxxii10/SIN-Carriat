fc5ba62aba6115937993b986f8341e1a88059d57
Inkative <strycliefnaf@gmail.com>
2024-10-07 09:18:32 +0200

    tp-2 wassil

---
+++ Wassil/act2/1.2 exercice1.py
@@ -0,0 +1,14 @@
+#Les boucles
+
+#Les boucles ireratives
+for voyelle in ['a','e','i','o','u','y']:
+    print (voyelle)
+
+for n in range (10):
+    print(n)
+
+for n in range(2,7):
+    print(n)
+
+for pair in range(100,110,2):
+    print(pair)
