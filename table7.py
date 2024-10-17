bcc9e7eac08d8535d165ac961e64e81b35598303
Lucas <luxxiiglitcher@gmail.com>
2024-10-17 11:03:56 +0200

    tp4 lulu

--- table7.py
+++
@@ -1,9 +0,0 @@
-# Table de multiplication par 7
-
-def table7():  # Définition de la fonction
-    n = 1
-    while n <11 : #condition pour faire 10x N*7 et montrer le resultat a chaque étape
-        print(n*7)
-        n = n +1
-
-table7 () # Appel de la fonction
